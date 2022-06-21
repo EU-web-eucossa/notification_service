cfad9fcad024a3f8ed6c146c632db46c0b063bd9
otienochris <otienochris98@gmail.com>
2022-06-21 10:36:34 +0300

    adding and setting up dependencies

---
+++ src/main/java/com/eucossa/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,17 @@
+package com.eucossa.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/eucossa/notification_service/configs/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.eucossa.notification_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author christopherochiengotieno@gmail.com
+ * @version 1.0.0
+ * @since Tuesday, 21/06/2022
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
