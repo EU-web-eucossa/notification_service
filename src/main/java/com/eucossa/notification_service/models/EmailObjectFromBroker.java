2ffcaff81467d9f4cc74e80208238d573f1e78cf
otienochris <otienochris98@gmail.com>
2022-06-27 23:02:51 +0300

    debugging: receiving email from broker

--- src/main/java/com/eucossa/notification_service/models/EmailObjectFromBroker.java
+++ src/main/java/com/eucossa/notification_service/models/EmailObjectFromBroker.java
@@ -1,11 +1,13 @@
 package com.eucossa.notification_service.models;
 
+import com.eucossa.notification_service.entities.EmailAttachment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,11 +22,11 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmailObject {
-    private UUID id;
+    private BigDecimal id;
     private String[] cc;
     private String[] bcc;
     private String[] emailTo;
     private String subject;
     private String messageInHtml;
-    private List<MultipartFile> attachments;
+    private List<EmailAttachment> attachments;
 }
