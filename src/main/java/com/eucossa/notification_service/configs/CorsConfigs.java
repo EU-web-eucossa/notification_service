83d7eb067d9d9b2fb4518b680bbd36aa79dd11bd
otienochris <otienochris98@gmail.com>
2022-07-11 11:17:19 +0300

    adding cors configs

---
+++ src/main/java/com/eucossa/notification_service/configs/CorsConfigs.java
@@ -0,0 +1,28 @@
+package com.eucossa.notification_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author christopherochiengotieno@gmail.com
+ * @version 1.0.0
+ * @since Monday, 11/07/2022
+ */
+@Configuration
+public class CorsConfigs {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "POST", "GET", "OPTION");
+            }
+        };
+    }
+
+}
